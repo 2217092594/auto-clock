74f20d20f603e41e303db1374b6378a18bda6e92
2217092594 <50054049+2217092594@users.noreply.github.com>
2022-02-16 09:54:21 +0800

    Update Start.java
    隐藏私密信息

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -12,15 +12,15 @@ import java.io.IOException;
 public class Start {
     public static void main(String[] args) throws IOException {
         Clock clock = new Clock();
-        String authorization = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcGVuaWQiOiJvV1JrVTBSbEtoRUdvNzhVbnZOUDA0YllsR3EwIiwidW5pb25pZCI6Im90Z3pwdndBTlJ1N1VKNkVxNzJsNm5vUEROVGciLCJwbGF0Zm9ybSI6ImFwcCIsImlhdCI6MTY0NDExNDkyMH0.yUlB1Wyye60AavnX2vMt6Nd4AVVXU2Ks_omdr8ECvAY";
+        String authorization = "抓包获取授权码";
         String receiveNickname = "你好，主人！！";
         String receiveMail = "2217092594@qq.com";
-        String new_name = "app_image_613081c6d510782a4a05ae04/52e5a923b750d8a89baf3650fe457f29_1644652871362.jpg";
+        String new_name = "抓包获取图片名称";
         clock.clock(authorization, receiveMail,receiveNickname,new_name);
-        String authorization1 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvcGVuaWQiOiJvV1JrVTBRa3B6YzlwQTJ0MExMaUxpZjJiRmp3IiwidW5pb25pZCI6Im90Z3pwdl96WmtIQjdPM1gwTW1zTVlOeFZ0bVEiLCJwbGF0Zm9ybSI6ImFwcCIsImlhdCI6MTY0NDkzNjE4MX0.-bV1uzksJgS15Xer8Z4abMYRfHEhDZs4eSH-8M9ZFw8";
-        String receiveNickname1 = "范彦博先生";
+        String authorization1 = "抓包获取授权码";
+        String receiveNickname1 = "xxx";
         String receiveMail1 = "741313672@qq.com";
-        String new_name1 = "11oWRkU0Qkpzc9pA2t0LLiLif2bFjw_img/YnL6_cos@186.jpg";
+        String new_name1 = "抓包获取图片名称";
         clock.clock(authorization1, receiveMail1,receiveNickname1,new_name1);
 
     }
